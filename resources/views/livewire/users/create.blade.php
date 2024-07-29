2a28244b0dc2547de9bd3c89906109b8549d2bbf
Fady G Andrawes <fadyelgendy2017@gmail.com>
2024-07-29 10:45:30 +0300

    create user

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -1,11 +1,102 @@
 <?php
 
+use App\Models\User;
+use App\Models\Language;
 use Livewire\Volt\Component;
+use Mary\Traits\Toast;
+use Livewire\Attributes\Validate;
+use App\Models\Country;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Hash;
 
 new class extends Component {
-    //
+    use Toast, WithFileUploads;
+
+    #[Validate('required')]
+    public string $name;
+
+    #[Validate('required|email')]
+    public string $email;
+
+    #[Validate('required|email')]
+    public string $password;
+
+    #[Validate('sometimes')]
+    public ?int $country_id;
+
+    #[Validate('nullable|image|max:1024')]
+    public $photo;
+
+    #[Validate('required')]
+    public array $my_languages = [];
+
+    #[Validate('sometimes')]
+    public ?string $bio = null;
+
+    public function save(): void
+    {
+        $data = $this->validate();
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        if ($this->photo) {
+            $url = $this->photo->store('users', 'public');
+            $user->update(['avatar' => "/storage/$url"]);
+        }
+
+        // Sync selection
+        $user->languages()->sync($this->my_languages);
+
+        $this->success('User created with Success', redirectTo: '/users');
+    }
+
+    public function with(): array
+    {
+        return [
+            'countries' => Country::get(),
+            'languages' => Language::all(),
+        ];
+    }
 }; ?>
 
 <div>
-    //
+    <x-header title="Create User" separator />
+
+    <x-form wire:submit='save'>
+        <div class="grid-cols-5 lg:grid">
+            <div class="col-span-2">
+                <x-header title="Basic" subtitle="Basic info from user" size="text-2xl" />
+            </div>
+            <div class="grid col-span-3 gap-3">
+                <x-file label="Avatar" wire:model="photo" accept="image/png, image/jpeg" crop-after-change>
+                    <img src="{{ $user->avatar ?? '/empty-user.jpg' }}" class="h-40 rounded-lg" />
+                </x-file>
+
+                <x-input label="Name" wire:model='name' />
+                <x-input label="Email" wire:model='email' type="email" />
+                <x-input label="Password" wire:model='password' icon="o-eye" type="password" />
+                <x-select label="Country" wire:model='country_id' :options="$countries" placeholder="Select Country" />
+            </div>
+        </div>
+
+        {{--  Details section --}}
+        <hr class="my-5" />
+
+        <div class="grid-cols-5 lg:grid">
+            <div class="col-span-2">
+                <x-header title="Details" subtitle="More about the user" size="text-2xl" />
+            </div>
+            <div class="grid col-span-3 gap-3">
+                <x-choices-offline label="My languages" wire:model="my_languages" :options="$languages" searchable />
+                <x-editor wire:model="bio" label="Bio" hint="The great biography" />
+            </div>
+        </div>
+
+        <x-slot:actions>
+            <x-button label="Cancel" link="/users" />
+            <x-button label="save" icon="o-paper-airplane" spinner="save" type="submit" class="btn-primary" />
+        </x-slot:actions>
+    </x-form>
 </div>
